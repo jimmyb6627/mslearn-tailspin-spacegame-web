58faf0be6ebe8466c818c1067943941e9dd749b3
jimmyb6627 <Brown.James.S@gmail.com>
2022-08-23 09:30:38 -0700

    Update buildTest.sh

--- buildTest.sh
+++ buildTest.sh
@@ -1,1 +1,1 @@
-/usr/share/dotnet/dotnet build --no-incremental /p:UseSharedCompilation=false /home/azureuser/buildRepo/%build.vcs.number%/Tailspin.SpaceGame.Web.sln
+/usr/share/dotnet/dotnet build --no-incremental /p:UseSharedCompilation=false Tailspin.SpaceGame.Web.sln
